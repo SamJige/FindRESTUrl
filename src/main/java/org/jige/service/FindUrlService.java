28e2245b2ebc44b558218adabddc8c8c5b9bb650
yanghongyi <469945613@qq.com>
2020-05-19 17:13:29 +0800

    修改弹出框

--- src/main/java/org/jige/service/FindUrlService.java
+++ src/main/java/org/jige/service/FindUrlService.java
@@ -4,8 +4,12 @@ import com.intellij.navigation.ChooseByNameContributorEx;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonShortcuts;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.ComponentPopupBuilder;
+import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.Processor;
@@ -13,10 +17,9 @@ import com.intellij.util.indexing.FindSymbolParameters;
 import com.intellij.util.indexing.IdFilter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jige.bean.ListItem;
-import org.jige.bean.MyList;
 
-import java.util.Arrays;
+import javax.swing.*;
+import java.awt.*;
 
 public class FindUrlService extends AnAction implements ChooseByNameContributorEx {
 
@@ -24,13 +27,95 @@ public class FindUrlService extends AnAction implements ChooseByNameContributorE
     public void actionPerformed(AnActionEvent e) {
         //获取当前在操作的工程上下文
         Project project = e.getData(PlatformDataKeys.PROJECT);
-        MyList list = new MyList("Find Result", Arrays.asList(
-                new ListItem("aa")
-                , new ListItem("bb")
-                , new ListItem("cc")
-                , new ListItem("dd")
-        ));
-        JBPopupFactory.getInstance().createListPopup(list).showInBestPositionFor(e.getDataContext());
+        createPopup().showInBestPositionFor(e.getDataContext());
+    }
+
+//    @NotNull
+//    JBPopup createPopupBak() {
+//        JPanel panel = new JPanel(new BorderLayout());
+//        panel.add(myTextField, BorderLayout.CENTER);
+//        ComponentPopupBuilder builder = JBPopupFactory.getInstance().createComponentPopupBuilder(panel, myTextField)
+//                .setCancelOnClickOutside(true)
+//                .setAdText(KeymapUtil.getShortcutsText(CommonShortcuts.CTRL_ENTER.getShortcuts()) + " to finish")
+//                .setRequestFocus(true)
+//                .setResizable(true)
+//                .setMayBeParent(true);
+//
+//        final JBPopup popup = builder.createPopup();
+//        popup.setMinimumSize(new Dimension(200, 90));
+//        AnAction okAction = new DumbAwareAction() {
+//            @Override
+//            public void actionPerformed(@NotNull AnActionEvent e) {
+//                unregisterCustomShortcutSet(popup.getContent());
+//                popup.closeOk(e.getInputEvent());
+//            }
+//        };
+//        okAction.registerCustomShortcutSet(CommonShortcuts.CTRL_ENTER, popup.getContent());
+//        return popup;
+//    }
+
+
+    /**
+     * https://www.programcreek.com/java-api-examples/index.php?api=com.intellij.openapi.ui.popup.JBPopup
+     */
+    @NotNull
+    JBPopup createPopup() {
+        JTextField jTextField1 = new JTextField();
+        JScrollPane jScrollPane1 = new JScrollPane();
+        JList<String> jList1 = new JList<>();
+
+        JPanel panel = new JPanel(new BorderLayout());
+
+        jScrollPane1.setViewportView(jList1);
+        jList1.setModel(new javax.swing.AbstractListModel<>() {
+            String[] strings = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+
+            public int getSize() {
+                return strings.length;
+            }
+
+            public String getElementAt(int i) {
+                return strings[i];
+            }
+        });
+        /////////////////////////////////////////////////////////////
+        // 构造弹出框 使用netbeans构造再copy
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(panel);
+        panel.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jTextField1)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 610, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 334, Short.MAX_VALUE))
+        );
+        /////////////////////////////////////////////////////////////
+
+        ComponentPopupBuilder builder = JBPopupFactory.getInstance().createComponentPopupBuilder(panel, null)
+                .setCancelOnClickOutside(true)
+//                .setAdText(KeymapUtil.getShortcutsText(CommonShortcuts.CTRL_ENTER.getShortcuts()) + " to finish")
+                .setRequestFocus(true)
+                .setResizable(true)
+                .setMayBeParent(true);
+
+        final JBPopup popup = builder.createPopup();
+        popup.setMinimumSize(new Dimension(200, 90));
+        AnAction okAction = new DumbAwareAction() {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+                unregisterCustomShortcutSet(popup.getContent());
+                popup.closeOk(e.getInputEvent());
+            }
+        };
+        okAction.registerCustomShortcutSet(CommonShortcuts.CTRL_ENTER, popup.getContent());
+        return popup;
     }
 
     @Override
