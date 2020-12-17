d55d82fa4a89080acc3b119c94befff5ead3d67b
yanghongyi <469945613@qq.com>
2020-12-17 12:06:33 +0800

    修改了@Deprecated的代码

--- src/main/java/org/intellij/sdk/language/SimpleIcons.java
+++ src/main/java/org/intellij/sdk/language/SimpleIcons.java
@@ -7,5 +7,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class SimpleIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.findIcon("/icons/jar-gray.png");
 }
--- src/main/java/org/jige/service/MyNotifier.java
+++ src/main/java/org/jige/service/MyNotifier.java
@@ -1,14 +1,14 @@
 package org.jige.service;
 
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
 import com.intellij.openapi.project.Project;
+import org.slf4j.LoggerFactory;
 
 public class MyNotifier {
-    private final NotificationGroup NOTIFICATION_GROUP = new NotificationGroup("Find REST Url logs", NotificationDisplayType.BALLOON, true);
-
+    //    private final NotificationGroup NOTIFICATION_GROUP = new NotificationGroup("Find REST Url logs", NotificationDisplayType.BALLOON, true);
+//
     public void notify(Project project, String content) {
-//        final Notification notification = NOTIFICATION_GROUP.createNotification(content, NotificationType.INFORMATION);
-//        notification.notify(project);
+////        final Notification notification = NOTIFICATION_GROUP.createNotification(content, NotificationType.INFORMATION);
+////        notification.notify(project);
+        LoggerFactory.getLogger(getClass()).info("just simple log -> {}", content);
     }
 }
